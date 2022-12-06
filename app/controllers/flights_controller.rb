68262a21ef23d0535174b51dedc820e40fd05516
atai <atai.isaev7@gmail.com>
2022-12-06 16:58:32 -0500

    Search Working

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,21 @@
 class FlightsController < ApplicationController
   def index
+    if params[:flight] 
+      if params[:flight][:departure_id] != '' && params[:flight][:arrival_id] != '' && params[:flight][:shuppatsu] != ''
+        @results = Flight.where(departure_id: params[:flight][:departure_id], arrival_id: params[:flight][:arrival_id], shuppatsu: params[:flight][:shuppatsu])
+      elsif params[:flight][:departure_id] != '' && params[:flight][:arrival_id] != ''
+        @results = Flight.where(departure_id: params[:flight][:departure_id], arrival_id: params[:flight][:arrival_id])
+        @a = "glargalarg"
+      elsif params[:flight][:departure_id] != ''
+        @results = Flight.where(departure_id: params[:flight][:departure_id])
+      elsif params[:flight][:arrival_id] != ''
+        @results = Flight.where(arrival_id: params[:flight][:arrival_id])
+      end
+    else 
+      
+    end
     
+    
+
   end
 end
